1a70b57b074a747fc4e269659f5506262031e067
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-27 14:36:57 -0500

    Implement BooksList container

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
 
-const BooksList = () => (
-  <div />
+const BooksList = ({ books }) => (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          books.map(
+            (book) => (<Book key={book.id} book={book} />),
+          )
+        }
+      </tbody>
+    </table>
+  </div>
 );
+
+BooksList.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  books: PropTypes.array.isRequired,
+};
 
 export default BooksList;
