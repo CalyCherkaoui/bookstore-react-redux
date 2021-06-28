cf24effb89e6237910e2d8be95b52da619c57587
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-28 10:09:13 -0500

    Implement BooksForm container

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,29 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
-const BooksForm = () => (
-  <div />
-);
+const BooksForm = () => {
+  const categoriesList = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Title" name="title" id="title" />
+
+        <label>
+          Select a category :
+          <select name="category" id="category">
+            {
+              categoriesList.map(
+                (category) => (
+                  <option key="category" value={category}>{category}</option>
+                ),
+              )
+            }
+          </select>
+        </label>
+
+      </form>
+    </div>
+  );
+};
 
 export default BooksForm;
