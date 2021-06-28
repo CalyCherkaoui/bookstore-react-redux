fb15ba2d686f80638d312ebbd6ea5f96db2769a9
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-28 11:34:06 -0500

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './reducers/index';
-import { createStore } from 'redux';
 
 const initialState = {
   books: [
@@ -16,11 +17,13 @@ const initialState = {
   ],
 };
 
-const store = createStore( rootReducer, initialState);
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
